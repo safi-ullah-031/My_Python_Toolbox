15cee8bf10a5d4d7c966770a3aade4173d18e794
Safi Ullah <safiullah445@outlook.com>
2025-03-07 17:50:48 +0500

    update

--- keylogger_detector.py
+++ keylogger_detector.py
@@ -140,3 +140,15 @@ def create_ui():
 
 # Run the Application
 create_ui()
+
+
+
+def create_ui():
+    """Builds the GUI for the tool."""
+    global root, result_textbox
+
+    # Main Window
+    root = ctk.CTk()
+    root.title("🔍 Keylogger Detector")
+    root.geometry("500x500")
+    root.resizable(False, False)
